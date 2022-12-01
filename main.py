e3c475a2e0ad169d9247c958fea461dcc9ed5ea8
Alexander Valiev <118052965+Valievx@users.noreply.github.com>
2022-12-01 22:49:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,87 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from auth_data import vk_password, vk_login, token, playlist_id
 import time
+import json
+from threading import *
 
 
-# options
-options = webdriver.ChromeOptions()
-options.add_argument("user-data-dir=https://") # ссылка личной страницы
-options.add_argument("user-agent=VKAndroidApp/4.38-849 (Android 6.0; SDK 23; x86; Google Nexus 5X; ru")
-s = Service("/Users/valiev/code/python/vkbot_playlist/chromedriver/chromedriver")
-driver = webdriver.Chrome(service=s, options=options)
-
-'''
-# Authorization
-
-driver.get("https://vk.com/")
-time.sleep(5)
-
-# login
-email_input = driver.find_element("id", "index_email")
-email_input.clear()
-email_input.send_keys(vk_login)
-time.sleep(5)
-
-login_button = driver.find_element(By.XPATH, "//*[@id='index_login']/div[1]/form/button[1]").click()
-time.sleep(5)
-
-# password
-password_input = driver.find_element(By.XPATH, "//*[@id='root']/div/div/div/div/div[2]/div/div/div/form/div[1]/div[3]/div[1]/div/input")
-password_input.clear()
-password_input.send_keys(vk_password)
-time.sleep(5)
-
-password_button = driver.find_element(By.XPATH, "//*[@id='root']/div/div/div/div/div[2]/div/div/div/form/div[2]/button").click()
-time.sleep(50)
-'''
-
-# Вход и открытие плейлиста
-
-driver.get(f"{playlist_id}")
-time.sleep(2)
-url = playlist_id
+'''options'''
+# options.add_argument("--headless")
 
 
-# Цикл
-amount = driver.find_element(By.XPATH, f'//*[@id="mcont"]/div/div/div[4]').text
-new_amount = amount[:-30]   # 30 # 32 # 34
-new_thing = int(new_amount) + 1
-print(new_thing)
+'''Authorization'''
+
+playlist_url = input("Введите URl playlist: ")
 
 
-for i in range(1, new_thing):
-    driver.find_element(By.XPATH, f'//*[@id="mcont"]/div/div/div[3]/div/div[{i}]').click()
-    time.sleep(36)
+with open("accounts.json", "r", encoding="utf-8") as file:
+    data = json.loads(file.read())
+    accounts = data["accounts"]
 
 
-driver.close()
-driver.quit()
+def auth_user():
+    for account in accounts:
+        try:
+            options = webdriver.ChromeOptions()
+            options.add_argument(f"user-data-dir={account['id']}")
+            options.add_argument("user-agent=VKAndroidApp/4.38-849 (Android 6.0; SDK 23; x86; Google Nexus 5X; ru")
+            # options.add_argument("--disable-blink-features=AutomationControlled")
+            s = Service("/Users/valiev/code/python/vkbot_playlist/chromedriver/chromedriver")
+            driver = webdriver.Chrome(service=s, options=options)
+
+            try:
+                ''' Save session'''
+                driver.get("https://m.vk.com/")
+                time.sleep(2)
+
+                '''btn-open'''
+                btn_open = driver.find_element(By.XPATH, "//*[@id='vkidform__signin']/a").click()
+                time.sleep(2)
+
+                '''login'''
+                email_input = driver.find_element(By.XPATH, "//*[@id='root']/div/div/div/div/div[2]/div/div/div/form/div[1]/section/div[1]/div/div/input")
+                email_input.clear()
+                email_input.send_keys(account['phone'])
+                time.sleep(2)
+
+                login_button = driver.find_element(By.XPATH, "//*[@id='root']/div/div/div/div/div/div/div/div/form/div[2]/div[1]/button/span[1]").click()
+                time.sleep(10)
+
+                '''password'''
+                password_input = driver.find_element(By.XPATH, "//*[@id='root']/div/div/div/div/div[2]/div/div/div/form/div[1]/div[3]/div[1]/div/input")
+                password_input.clear()
+                password_input.send_keys(account['password'])
+                time.sleep(2)
+
+                password_button = driver.find_element(By.XPATH, "//*[@id='root']/div/div/div/div/div[2]/div/div/div/form/div[2]/button").click()
+                time.sleep(2)
+            except Exception as ex:
+                pass
+
+            driver.get(f"{playlist_url}")
+            time.sleep(2)
+
+            amount = driver.find_element(By.XPATH, f'//*[@id="mcont"]/div/div/div[4]').text
+            new_amount = amount[:2]   # 2(1-99) 3(100-999)
+            new_thing = int(new_amount) + 1
+            print(new_thing)
+
+            for i in range(1, new_thing):
+                driver.find_element(By.XPATH, f'//*[@id="mcont"]/div/div/div[3]/div/div[{i}]').click()
+                time.sleep(36)
+                driver.find_element(By.XPATH, f'//*[@id="mcont"]/div/div/div[1]/div[1]/div[2]/div/div/div[1]/a').click()
+                time.sleep(2)
+
+        except Exception as ex:
+            print(ex)
+        finally:
+            driver.close()
+            driver.quit()
+
+
+if __name__ == '__main__':
+    p1 = Thread(target=auth_user)
+
+    p1.start()
+    p1.join()
